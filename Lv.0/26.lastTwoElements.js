00c05f5d7391fba858c218f614b36a4974150486
Samyeol Son <samyeol0930@gmail.com>
2023-12-18 21:44:33 +0900

    [complete] lastTwoElements.js

---
+++ Lv.0/26.lastTwoElements.js
@@ -0,0 +1,9 @@
+function solution(num_list) {
+	if (num_list.at(-1) > num_list.at(-2)) {
+		num_list.push(num_list.at(-1) - num_list.at(-2));
+		return num_list;
+	} else {
+		num_list.push(num_list.at(-1) * 2);
+		return num_list;
+	}
+}
